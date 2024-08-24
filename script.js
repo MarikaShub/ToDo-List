ed1514402f3a460f7f935ffbc0a752daa5034fd8
MarikaShub <city-sma@mail.ru>
2024-08-24 19:08:20 +0300

    saved margin

--- script.js
+++ script.js
@@ -9,24 +9,23 @@ addButton.addEventListener("click", function () {
   //присваиваем значение из поля ввода
   listItem.textContent = input.value;
   //воздаем кнопку "Удалить" для элемента списка
-  const deleteButton = document.createElement('button')
-  deleteButton.innerHTML = 'Удалить'
-/*deleteButton.setAttribute('id', 'button')*/ 
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Удалить";
+  /*deleteButton.setAttribute('id', 'button')*/
   //возможность удалить элемент списка по клику на кнопку
   deleteButton.onclick = () => {
-    listItems.removeChild(listItem)
-  }
+    listItems.removeChild(listItem);
+  };
   //добавляем элемент списка и кнопку в список
   listItems.append(listItem);
-  listItem.append(deleteButton)
+  listItem.append(deleteButton);
   input.value = "";
 });
 //Добавляем обработчик чтобы отметить задачу, как выполненную
 
-listItems.addEventListener('click', function(evt) {
-    if (evt.target.tagName === 'LI') {
-      // Код, который будет выполнен при клике на элемент <li>
-      evt.target.classList.toggle('completed'); // переключение класса
-
-    }
-  });
+listItems.addEventListener("click", function (evt) {
+  if (evt.target.tagName === "LI") {
+    // Код, который будет выполнен при клике на элемент <li>
+    evt.target.classList.toggle("completed"); // переключение класса
+  }
+});
